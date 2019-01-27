b034eb1a5137b898b2e2fd44a18ceff4d365614c
andreb86 <andrea.basso.cpp@gmail.com>
2019-01-28 00:40:01 +0100

    Updated with the Allreduce

--- main.cpp
+++ main.cpp
@@ -20,26 +20,23 @@ int main(int argc, char **argv) {
     // Initialise MPI session and relevant variables
     int nodes, rank;
     unsigned int m, n, c, b, r, block; // feature space, n datapoints, c centroids, batch size, remainder and block
-    float e, f; // tolerance for the convergence of the problem and fuzzification parameter
-    int *sendcounts, *displs; // arguments for the scatterv function
-    double *X;
-
-    // TEST BUFFER
-    int recv_buf[100];
+    double e, f; // tolerance for the convergence of the problem and fuzzification parameter
+    int *counts, *displs; // arguments for the scatterv function
 
     Data *d;
     MPI_Init(&argc, &argv);
     MPI_Status stat;
     MPI_Comm_size(MPI_COMM_WORLD, &nodes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double *y, *usum;
 
     // Initialise the array of sizes and the array of the offsets from beginning
-    sendcounts = (int *)malloc(nodes * sizeof(int));
-    displs     = (int *)malloc(nodes * sizeof(int));
+    counts = (int *)malloc(nodes * sizeof(int));
+    displs = (int *)malloc(nodes * sizeof(int));
 
     // Initialise with the CLI arguments
     c = static_cast<unsigned int>(std::stoi(argv[2])); // number of centroids for the problem
-    block = static_cast<unsigned int>(std::stoi(argv[5])); // block size
+    block = static_cast<const unsigned int>(std::stoi(argv[5])); // block size
     f = std::stof(argv[3]); // fuzzification parameter
     e = std::stof(argv[4]); // tolerance
 
@@ -57,7 +54,7 @@ int main(int argc, char **argv) {
         }
 
         // Handle incorrect input
-        if (argc != 5) {
+        if (argc != 6) {
             std::cerr << "Unexpected number of arguments provided" << std::endl;
             std::cout << help << std::endl;
             MPI_Abort(MPI_COMM_WORLD, argc);
@@ -67,7 +64,9 @@ int main(int argc, char **argv) {
         d = new Data(argv[1]);
         n = d->get_size();
         m = d->get_dim();
-//        X = d->dataset();
+        d->init_centroids(c);
+        y = d->centroids();
+
 
         // Calculate the dimension of the buffer that will hold the scattered dataset
         b = n / nodes;
@@ -75,17 +74,17 @@ int main(int argc, char **argv) {
         std::cout << "================= P A R T I T I O N I N G ====================" << std::endl;
         for (int i = 0; i < nodes; ++i) {
             if (i < r) {
-                sendcounts[i] = (b + 1) * m;
+                counts[i] = (b + 1) * m;
             } else {
-                sendcounts[i] = b * m;
+                counts[i] = b * m;
             }
 
             if (i == 0) {
                 displs[i] = 0;
             } else {
-                displs[i] = displs[i - 1] + sendcounts[i - 1];
+                displs[i] = displs[i - 1] + counts[i - 1];
             }
-            std::cout << "Process: " << i << " N. elements: " << sendcounts[i] << " Offset: " << displs[i] << std::endl;
+            std::cout << "Process: " << i << " N. elements: " << counts[i] << " Offset: " << displs[i] << std::endl;
         }
     }
 
@@ -105,6 +104,14 @@ int main(int argc, char **argv) {
         exit(err);
     }
 
+    err = MPI_Bcast(y, c * m, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    if (err != MPI_SUCCESS) {
+        std::cerr << "Unable to send centroids!" << std::endl;
+        std::cerr << err << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, err);
+        exit(err);
+    }
+
     // Calculate the number of data points to be sent to each process
     b = n / nodes;
     r = n % nodes;
@@ -115,7 +122,7 @@ int main(int argc, char **argv) {
     // Create the clustering class for each process
     CMeans fcm = CMeans(b, m, c, f, e);
     std::cout.flush();
-    err = MPI_Scatterv(d->dataset(), sendcounts, displs, MPI_DOUBLE, fcm.x, b * m, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    err = MPI_Scatterv(d->dataset(), counts, displs, MPI_DOUBLE, fcm.x, b * m, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
     if (err != MPI_SUCCESS) {
         std::cerr.flush();
         std::cerr << "Unable to scatter data to processes!" << std::endl;
@@ -124,9 +131,24 @@ int main(int argc, char **argv) {
         exit(err);
     }
 
-    do {
-        fcm.distances()
-    }
+    // Copy the centroids into the aligned buffer
+    std::memcpy(fcm.y, y, c * m * sizeof(double));
+    double diff;
+
+//    do {
+//        fcm.distances(block);
+//        fcm.weights(block);
+//        diff = fcm.check(block);
+//        fcm.umulx();
+//        MPI_Allreduce(fcm.y, y, c * m, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//        MPI_Allreduce(fcm.u_sum, usum, c, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+//        for (int i = 0; i < c; ++i) {
+//            for (int j = 0; j < m; ++j) {
+//                y[i * m + j] /= usum[i];
+//            }
+//        }
+//        std::memcpy(fcm.y, y, c * m * sizeof(double));
+//    } while (diff > e);
 
 
 
--- src/CMeans.h
+++ src/CMeans.h
@@ -12,7 +12,7 @@
 #include <algorithm>
 #include <omp.h>
 #include <mm_malloc.h>
-#include </home/andrea/amd/blis/include/blis/cblas.h>
+//#include </home/andrea/amd/blis/include/blis/cblas.h>
 
 class CMeans {
 public:
@@ -21,10 +21,14 @@ public:
     const double f, e; // fuzzification parameter and tolerance
 
     // points, centroids coordinates, distances and the membership vector
-    double *x, *y, *d, *u_old, *u_new, *u_sum;
+    double *x, *y, *d, *d_sum, *u_old, *u_new, *u_pow, *u_sum;
 
     // initialise the value of the batch size b, number of centroids c and dimension of the problem
-    CMeans(const unsigned bb, const unsigned mm, const unsigned cc, const double ff, const double ee):
+    CMeans(const unsigned bb,
+            const unsigned mm,
+            const unsigned cc,
+            const double ff,
+            const double ee):
     b(bb), c(cc), m(mm), f(ff), e(ee) {
 
         // Allocate aligned memory
@@ -33,8 +37,10 @@ public:
         x     = static_cast<double *>(_mm_malloc(sizeof(double) * bb * mm, CACHELINE));
         y     = static_cast<double *>(_mm_malloc(sizeof(double) * cc * mm, CACHELINE));
         d     = static_cast<double *>(_mm_malloc(sizeof(double) * bb * cc, CACHELINE));
+        d_sum = static_cast<double *>(_mm_malloc(sizeof(double) * bb *  1, CACHELINE));
         u_old = static_cast<double *>(_mm_malloc(sizeof(double) * cc * bb, CACHELINE));
         u_new = static_cast<double *>(_mm_malloc(sizeof(double) * cc * bb, CACHELINE));
+        u_pow = static_cast<double *>(_mm_malloc(sizeof(double) * cc * bb, CACHELINE));
         u_sum = static_cast<double *>(_mm_malloc(sizeof(double) * cc *  1, CACHELINE));
 
         if (x == nullptr || y == nullptr || d == nullptr || u_old == nullptr || u_new == nullptr || u_sum == nullptr) {
@@ -42,8 +48,10 @@ public:
             _mm_free(x);
             _mm_free(y);
             _mm_free(d);
+            _mm_free(d_sum);
             _mm_free(u_old);
             _mm_free(u_new);
+            _mm_free(u_pow);
             _mm_free(u_sum);
             exit(1);
         }
@@ -51,82 +59,93 @@ public:
         // initialise the membership vectors
 #pragma omp simd
         for (int k = 0; k < b * c; ++k) {
-            u_new[k] = u_old[k] = 1 / c;
+            u_new[k] = 1;
+            u_pow[k] = 1;
+            u_old[k] = 1 / c;
         }
     }
 
     // calculate the square of the distances
     void distances(const unsigned int block_size) {
         double tmp[m] __attribute__((aligned (CACHELINE)));
-#pragma omp parallel for schedule(static) private(tmp) num_threads(nthreads)
+        double tmp_d;
+#pragma omp parallel for schedule(static) private(tmp, tmp_d) num_threads(nthreads)
         for (int ii = 0; ii < b; ii += block_size) {
             int ib = ii + block_size;
-            for (int j = 0; j < c; ++j)
-                for (int i = ii; i < std::min(ib, b); ++i) {
-                    d[i * c + j] = 0;
+            for (int i = ii; i < std::min(ib, b); ++i) {
+                d_sum[i] = 0;
+                for (int j = 0; j < c; ++j) {
+                    tmp_d = 0;
 #pragma omp simd
                     for (int k = 0; k < m; ++k)
                         tmp[k] = std::pow(x[i * m + k] - y[j * m + k], 2);
                     for (int kk = 0; kk < m; ++kk)
-#pragma omp atomic
-                        d[i * c + j] += tmp[kk];
+                        tmp_d += tmp[kk];
+                    d[i * c + j] = std::pow(tmp_d, 1 / (1 - f));
+                    d_sum[i] += d[i * c + j];
                 }
+            }
         }
     }
 
     // calculate the weights of each data point as ownership of a certain centroid
     void weights(const unsigned int block) {
-        double sum, d1;
-#pragma omp parallel for schedule(static) num_threads(nthreads)
+        double tmp;
+#pragma omp parallel for schedule(static) private(tmp) num_threads(nthreads)
         for (int ii = 0; ii < b; ii += block) {
             int ib = ii + block;
             for (int j = 0; j < c; ++j) {
+                tmp = 0;
                 for (int i = ii; i < std::min(ib, b); ++i) {
-                    sum = 0.0;
-                    d1 = std::pow(d[i * c + j], 1 / (1 - f));
-                    for (int k =  0; k < c; ++k) {
-                        sum += std::pow(d[i * c + k], 1 / (1 - f));
-                    }
-                    u_new[j * b + i] = d1 / sum;
-
+                    u_new[j * b + i] = d[i * c + j] / d_sum[i];
+                    u_pow[j * b + i] = std::pow(u_new[j * b + i], f);
+                    tmp += u_pow[j * b + i];
                 }
+                u_sum[j] = tmp;
             }
         }
     }
 
-    void usums(const unsigned int block) {
-#pragma omp parallel for schedule(static) num_threads(nthreads)
+    double check(const unsigned int block) {
+        double err = 0, tmp;
+#pragma omp parallel for schedule(static) private(tmp) num_threads(nthreads) reduction(+:err)
         for (int ii = 0; ii < b; ii += block) {
-            int ib = ii + block;
-            for (int j = 0; j < c; ++j) {
-                u_sum[j] = 0;
-                for (int i = ii; i < std::min(ib, b); ++i) {
-                    u_sum[j] += u_new[j * b + i];
-                }
-            }
-        }
-    }
-
-    bool check(const unsigned int block) {
-        double err = 0;
-        bool res = false;
-#pragma omp parallel for schedule(static) num_threads(nthreads)
-        for (int ii = 0; ii < b; ii += block) {
+            tmp = 0;
             int ib = ii + block;
             for (int j = 0; j < c; ++j) {
                 for (int i = ii; i < std::min(ib, b); ++i) {
-                    err += std::pow(u_new[j * b + i] - u_old[j * b + i], 2);
-                    if (err >= e) {
-                        res = true;
-                    }
+                    tmp += std::pow(u_new[j * b + i] - u_old[j * b + i], 2);
+                }
+            }
+            err += tmp;
+        }
+        err = std::sqrt(err);
+        if (err > e) {
+            // Swap the old and new values of the membership vectors
+            double *tmp_p = u_new;
+            u_old = u_new;
+            u_new = tmp_p;
+        }
+        return err;
+    }
+
+    void umulx(const unsigned int block) {
+//        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, c, m, b, 1, u_pow, b, x, m, 0, y, m);
+        for (int ii = 0; ii < c; ii += block) {
+            int ib = ii + block;
+            for (int kk = 0; kk < b; kk += block) {
+                int kb = kk + block;
+                for (int jj = 0; jj < m; jj += block) {
+                    int jb = jj + block;
+                    for (int i = ii; i < std::min(ib, c); ++i)
+                        for (int k = kk; k < std::min(kb, b); ++k)
+                            for (int j = jj; j < std::min(jb, m); ++j) {
+                                if (!k) y[i * m + j] = 0;
+                                y[i * m + j] += u_old[i * b + k] * x[k * m + j];
+                            }
                 }
             }
         }
-        return res;
-    }
-
-    void update() {
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, c, m, b, 1, u_old, b, x, m, 0, y, m);
     }
 
     ~CMeans() {
@@ -135,9 +154,20 @@ public:
         _mm_free(x);
         _mm_free(y);
         _mm_free(d);
+        _mm_free(d_sum);
         _mm_free(u_old);
         _mm_free(u_new);
+        _mm_free(u_pow);
         _mm_free(u_sum);
+        x     = nullptr;
+        y     = nullptr;
+        d     = nullptr;
+        d_sum = nullptr;
+        u_old = nullptr;
+        u_new = nullptr;
+        u_pow = nullptr;
+        u_sum = nullptr;
+
         std::cout.flush();
         std::cout << "Bye!" << std::endl;
     }
